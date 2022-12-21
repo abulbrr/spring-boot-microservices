1485decf544c33b3ac91b82d101d7b4fdfed7954
Bara Habbal <bara.taufik@westernacher.com>
2022-12-21 02:41:14 +0200

    removed iml file

--- order-service/src/main/java/com/habbal/orderservice/client/InventoryClient.java
+++ order-service/src/main/java/com/habbal/orderservice/client/InventoryClient.java
@@ -2,10 +2,8 @@ package com.habbal.orderservice.client;
 
 import com.habbal.orderservice.dto.InventoryResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
